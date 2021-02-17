a7809382c0ef654fe7d13100eeaacda1b98a5e65
Chris-Eminence <emichris380@gmail.com>
2021-02-18 00:00:52 +0100

    dashboard on construction

--- app/src/main/java/com/example/acmat/LoginActivity.kt
+++ app/src/main/java/com/example/acmat/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.acmat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+    fun login(view: View) {
+
+        val intent = Intent(this, DashBoard::class.java)
+        startActivity(intent)
+    }
+
 }
--- app/src/main/java/com/example/acmat/SignUp.kt
+++ app/src/main/java/com/example/acmat/SignUp.kt
@@ -1,11 +1,18 @@
 package com.example.acmat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+    }
+
+    fun signUp(view: View) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
